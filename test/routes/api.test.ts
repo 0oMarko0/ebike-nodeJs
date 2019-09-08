8ea3567ec3d7d52cdeeb67533b693cc8b7888154
Marco <marc.olivier.gagnon.4@gmail.com>
2019-09-08 15:51:36 -0400

    fix(mocking-call): adding test for heartbeat

--- test/routes/api.test.ts
+++ test/routes/api.test.ts
@@ -1,12 +1,29 @@
 import request from "supertest";
 import app from "../../src/app";
+import chai from "chai";
 
-describe("GET /api/*", () => {
+const expect = chai.expect;
+
+describe("GET /api/heartbeat", () => {
     const HEART_BEAT_ROUTE = "/api/heartbeat";
 
     it("/heartbeat should return 200 OK", () => {
-        return request(app)
-            .get("/api/heartbeat")
+        request(app)
+            .get(HEART_BEAT_ROUTE)
             .expect(200);
     });
+
+    it("/heartbeat should have a valid response", () => {
+        request(app)
+            .get(HEART_BEAT_ROUTE)
+            .expect(200)
+            .end((err, res) => {
+                expect(res.body).to.have.property("nb_restaurants");
+                expect(res.body).to.have.property("total_path_length");
+            });
+    });
+});
+
+describe("GET /api/heartbeat", () => {
+
 });
