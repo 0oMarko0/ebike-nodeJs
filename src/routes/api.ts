1fb0677f48cb0b041a4ee26c514aee2c611f2475
Marc-Olivier Gagnon mogagnon@coveo.com <mogagnon@coveo.com>
2019-09-07 21:19:09 -0400

    feat(travis): testing travis

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/status", (req, res) => {
-    res.send("Server up - i");
+    res.send("Server up - Test");
 });
 
 export default router;
