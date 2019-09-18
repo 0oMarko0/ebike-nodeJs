98bcbf435d3516158c12a37efc055e3457aa291d
Marco <marc.olivier.gagnon.4@gmail.com>
2019-09-17 20:42:13 -0400

    feat(data): change bcrypt to bcryptjs

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import UserRepo from "../data/repository/user-repo";
 import User from "../model/user";
 import { Token } from "./controller";
