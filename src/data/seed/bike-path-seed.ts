4ac8f2ae9cce88a7b06c3a5a11caff9e30f07f02
Marc-Olivier <marc-olivier.gagnon.4@ulaval.ca>
2019-10-19 16:53:27 -0400

    feat(restaurant): adding restaurant
    feat(restaurant): adding restaurant

--- src/data/seed/bike-path-seed.ts
+++ src/data/seed/bike-path-seed.ts
@@ -1,6 +1,7 @@
 import ReadFile from "./readFile";
 import BikePathRepo from "../repository/bike-path";
 import logger from "../../utils/logger";
+import { log } from "util";
 
 export default class BikePathSeed extends ReadFile {
     private bikePathRepo: BikePathRepo;
@@ -12,7 +13,11 @@ export default class BikePathSeed extends ReadFile {
     async start() {
         const bikePath = this.readFromfile("sherbrooke_bike_path.geojson");
         await this.bikePathRepo.drop();
-        await this.bikePathRepo.create(Object.assign(bikePath, { city: "Sherbrooke" }));
+        try {
+            await this.bikePathRepo.create(Object.assign(bikePath, { city: "Sherbrooke" }));
+        } catch (e) {
+           logger.error(e.message);
+        }
         logger.info("SEEDING bike-path DONE");
     }
 }
