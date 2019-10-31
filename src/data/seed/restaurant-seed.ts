38df9a3cbcd2898c54b910f6251194de5f2997a5
Marco <marc.olivier.gagnon.4@gmail.com>
2019-10-30 23:15:20 -0400

    feature: removing Sherbrooke and adding Montreal restaurants

--- src/data/seed/restaurant-seed.ts
+++ src/data/seed/restaurant-seed.ts
@@ -1,20 +1,26 @@
 import logger from "../../utils/logger";
 import ReadFile from "./readFile";
 import RestaurantRepo from "../repository/restaurant-repo";
+import _ from "lodash";
 
 export default class RestaurantSeed extends ReadFile {
     private restaurantRepo: RestaurantRepo;
+
     constructor(restaurantRepo: RestaurantRepo) {
         super();
         this.restaurantRepo = restaurantRepo;
     }
 
     async start() {
-        const restaurants = this.readFromfile("sherbrooke_restaurant.json");
+        const file = this.readFromfile("montreal-restaurant.json");
+        const restaurants = file.elements;
         await this.restaurantRepo.drop();
         try {
-            Object.values(restaurants).map(async (row: any) => {
-                await this.restaurantRepo.create(this.translate(row, "Sherbrooke"));
+            Object.values(restaurants).map(async (document: any) => {
+                if (this.documentIsValid(document)) {
+                    const translated = this.translate(document);
+                    await this.restaurantRepo.create(translated);
+                }
             });
         } catch (e) {
             logger.error(e.message);
@@ -22,14 +28,51 @@ export default class RestaurantSeed extends ReadFile {
         logger.info("SEEDING restaurant DONE");
     }
 
-    private translate(row: any, city: string) {
-        return {
-            phone: row.phone,
-            coordinates: [row.latitude, row.longitude],
-            types: row.types,
-            placeId: row.placeID,
-            name: row.name,
-            address: row.address.full,
+    cleanAddress = (addr: string) => {
+        if (addr.includes("addr")) {
+            return addr.split(":")[1];
+        }
+    };
+
+    private documentIsValid(document: any): boolean {
+        const isvalid =
+            _.has(document, "lat") &&
+            _.has(document, "lon") &&
+            _.has(document, "tags.cuisine") &&
+            _.has(document, "tags.name");
+
+        return isvalid;
+    }
+
+    private translate(row: any) {
+        let address: any = {};
+        for (let key in row.tags) {
+            if (key.includes("addr")) {
+                address[this.cleanAddress(key)] = row.tags[key];
+                Object.assign(row, { address });
+                delete row.tags[key];
+            } else {
+                row[key] = row.tags[key];
+                delete row.tags[key];
+            }
+        }
+
+        const geometry = {
+            type: "Point",
         };
+        const coordinates = [];
+        coordinates.push(row.lon);
+        coordinates.push(row.lat);
+        Object.assign(geometry, { coordinates });
+        Object.assign(row, { geometry });
+
+        delete row.tags;
+        delete row.id;
+        delete row.type;
+        delete row.lat;
+        delete row.lon;
+        delete row.amenity;
+
+        return row;
     }
 }
